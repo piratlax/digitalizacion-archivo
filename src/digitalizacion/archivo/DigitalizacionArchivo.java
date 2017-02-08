e5d2c02686a55f714ceb11cd18ea6149e5798689
Dev <Dev@192.168.1.71>
2017-02-08 09:52:06 -0600

    Iniciando

---
+++ src/digitalizacion/archivo/DigitalizacionArchivo.java
@@ -0,0 +1,13 @@
+
+package digitalizacion.archivo;
+
+public class DigitalizacionArchivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
